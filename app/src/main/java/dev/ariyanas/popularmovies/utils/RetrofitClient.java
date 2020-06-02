321d87db8ccfb0f67fcb4dec545c2ccc1e816908
Arunabh Saikia <asaikia.143s@gmail.com>
2020-06-02 17:08:02 +0530

    Added interceptor, request builder to append api_key to url in RetrofitClient

---
+++ app/src/main/java/dev/ariyanas/popularmovies/utils/RetrofitClient.java
@@ -0,0 +1,47 @@
+package dev.ariyanas.popularmovies.utils;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String BASE_URL = "https://api.themoviedb.org/3/movie";
+
+    private static String MOVIEDB_API_KEY = System.getProperty("MOVIEDB_API_KEY");
+
+    private static Retrofit retrofitClient = null;
+
+    static Retrofit getClient() {
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request();
+
+                        HttpUrl reqUrl = request.url();
+
+                        HttpUrl newUrl = reqUrl.newBuilder()
+                            .addQueryParameter("api_key", MOVIEDB_API_KEY)
+                            .addQueryParameter("language", "en-US")
+                            .build();
+
+                        return chain.proceed(request.newBuilder().url(newUrl).build());
+                    }
+                })
+                .build();
+
+        retrofitClient = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build();
+
+        return retrofitClient;
+    }
+}
